6c44bd9049621ef02841c8a1711ed2219a8b79c7
Rick <Rick@DESKTOP-6VFJQ7F>
2023-11-07 17:52:18 -0300

    CDI

--- curso-jsf/src/main/java/repository/Empresas.java
+++ curso-jsf/src/main/java/repository/Empresas.java
@@ -3,6 +3,7 @@ package repository;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
@@ -11,7 +12,8 @@ import model.Empresa;
 public class Empresas implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@Inject
 	private EntityManager manager;
 
 	public Empresas() {
