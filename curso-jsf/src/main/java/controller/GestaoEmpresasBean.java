722c2a22ca3e9e9fc39d8a249f74f3bf2cb03010
Rick <Rick@DESKTOP-6VFJQ7F>
2023-11-15 17:32:47 -0300

    bean validation

--- curso-jsf/src/main/java/controller/GestaoEmpresasBean.java
+++ curso-jsf/src/main/java/controller/GestaoEmpresasBean.java
@@ -3,76 +3,111 @@ package controller;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.primefaces.PrimeFaces;
 
 import model.Empresa;
 import model.RamoAtividade;
 import model.TipoEmpresa;
 import repository.Empresas;
 import repository.RamoAtividades;
+import service.CadastroEmpresaService;
 import util.FacesMessages;
 
 @Named
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    
-    @Inject
-    private Empresas empresas;
-    
-    @Inject
-    private FacesMessages messages;
-    
-    @Inject
-    private RamoAtividades ramoAtividades;
-    
-    private List<Empresa> listaEmpresas;
-    
-    private String termoPesquisa;
-    
-    private Converter ramoAtividadeConverter;
-    
-    public void pesquisar() {
-        listaEmpresas = empresas.pesquisar(termoPesquisa);
-        
-        if (listaEmpresas.isEmpty()) {
-            messages.info("Sua consulta não retornou registros.");
-        }
-    }
-    
-    public void todasEmpresas() {
-        listaEmpresas = empresas.todas();
-    }
-    
-    public List<RamoAtividade> completarRamoAtividade(String termo){
-    	List<RamoAtividade> listaRamoAtividades = ramoAtividades.pesquisar(termo);
-    	
-    	ramoAtividadeConverter = new RamoAtividadeConverter(listaRamoAtividades);
-    	
-    	return listaRamoAtividades;
-    }
-    
-    public List<Empresa> getListaEmpresas() {
-        return listaEmpresas;
-    }
-    
-    public String getTermoPesquisa() {
-        return termoPesquisa;
-    }
-    
-    public void setTermoPesquisa(String termoPesquisa) {
-        this.termoPesquisa = termoPesquisa;
-    }
-    
-    public TipoEmpresa[] getTiposEmpresa() {
-    	return TipoEmpresa.values();
-    }
-    
-    public Converter getRamoAtividadeConverter() {
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Empresas empresas;
+
+	@Inject
+	private FacesMessages messages;
+
+	@Inject
+	private RamoAtividades ramoAtividades;
+
+	@Inject
+	private CadastroEmpresaService cadastroEmpresaService;
+
+	private List<Empresa> listaEmpresas;
+
+	private String termoPesquisa;
+
+	private Converter ramoAtividadeConverter;
+
+	private Empresa empresa;
+
+	public void prepararNovaEmpresa() {
+		empresa = new Empresa();
+	}
+
+	public void salvar() {
+		cadastroEmpresaService.salvar(empresa);
+
+		if (jaHouvePesquisa()) {
+			pesquisar();
+		}
+
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "Empresa cadastrada com sucesso!"));
+
+		PrimeFaces.current().executeScript("PF('successDialog').show();");
+	}
+
+	public void pesquisar() {
+		listaEmpresas = empresas.pesquisar(termoPesquisa);
+
+		if (listaEmpresas.isEmpty()) {
+			messages.info("Sua consulta não retornou registros.");
+		}
+	}
+
+	public void todasEmpresas() {
+		listaEmpresas = empresas.todas();
+	}
+
+	public List<RamoAtividade> completarRamoAtividade(String termo) {
+		List<RamoAtividade> listaRamoAtividades = ramoAtividades.pesquisar(termo);
+
+		ramoAtividadeConverter = new RamoAtividadeConverter(listaRamoAtividades);
+
+		return listaRamoAtividades;
+	}
+
+	private boolean jaHouvePesquisa() {
+		return termoPesquisa != null && !"".equals(termoPesquisa);
+	}
+
+	public List<Empresa> getListaEmpresas() {
+		return listaEmpresas;
+	}
+
+	public String getTermoPesquisa() {
+		return termoPesquisa;
+	}
+
+	public void setTermoPesquisa(String termoPesquisa) {
+		this.termoPesquisa = termoPesquisa;
+	}
+
+	public TipoEmpresa[] getTiposEmpresa() {
+		return TipoEmpresa.values();
+	}
+
+	public Converter getRamoAtividadeConverter() {
 		return ramoAtividadeConverter;
+	}
+
+	public Empresa getEmpresa() {
+		return empresa;
 	}
 }
